0f836c662f8a7e3701449df76903064dd3c02f16
Matt Behrens <askedrelic@gmail.com>
2011-08-15 20:17:16 -0400

    Cleanup, a few function refactorings, added ability to show today's journal entries

--- journal/main.py
+++ journal/main.py
@@ -1,38 +1,71 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import sys
 import argparse
-from datetime import datetime
 from os import path
+from datetime import datetime
+
+from journal import __version__
+
+DEST = ".journal"
 
 def parse_args():
     #parsing
-    parser = argparse.ArgumentParser(description='Journal some stuff', version='0.1')
-    parser.add_argument('message', action="store", help="message")
+    parser = argparse.ArgumentParser(
+            description='Simple CLI tool to help with keeping a work/personal journal',
+            version=__version__)
+    parser.add_argument('entry',
+            action="store",
+            help="Text to make an entry in your journal")
+    return parser, parser.parse_args()
 
-    args = parser.parse_args()
-    return args
-
-def record_message(message):
-    message_type = ".journal"
-
+def record_entry(entry):
     current_date = datetime.today()
     update_date = current_date.strftime("%a %I:%M:%S %Y-%m-%d")
-    message = update_date + "\n-" + message + "\n\n"
-    date_filename = path.expanduser("".join([
-            "~/",
-            message_type,
-            '/',
-            current_date.strftime("%Y.%m.%d"),
-            ".txt"]))
-    with open(date_filename, "a") as date_file:
-        date_file.write(message)
+    entry = update_date + "\n-" + entry + "\n\n"
+    with open(build_journal_path(current_date), "a") as date_file:
+        date_file.write(entry)
+
+def build_journal_path(date):
+    entry_dest = DEST
+    date_filename = path.expanduser("".join(
+        [ "~/", entry_dest, '/', date.strftime("%Y.%m.%d"), ".txt"]
+        ))
+    return date_filename
+
+def show_today():
+    current_date = datetime.today()
+    return show_entry(current_date)
+
+def show_entry(date):
+    """
+    args
+    date - datetime object
+    returns entry text or None if entry doesn't exist
+    """
+    try:
+        with open(build_journal_path(date), "r") as entry_file:
+            return entry_file.read()
+    except IOError:
+        return None
 
 def main():
     #parse args
-    args = parse_args()
-    #print args.message
-    record_message(args.message)
+    parser, args = parse_args()
+
+    if not str.strip(args.entry):
+        parser.print_help()
+        sys.exit()
+    elif args.entry == 'today':
+        entry = show_today()
+        if entry:
+            print entry
+        else:
+            print "journal: error: entry not found on that date"
+            sys.exit()
+    else:
+        record_entry(args.entry)
 
 if __name__ == "__main__":
     main()
