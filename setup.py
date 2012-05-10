d3d80101ee8f44f5be8006273e589513c78e0634
Matt Behrens <askedrelic@gmail.com>
2012-05-09 22:54:48 -0700

    more cleanup

--- setup.py
+++ setup.py
@@ -8,33 +8,31 @@ if sys.version_info[:2] < (2,6):
     required.append('argparse')
 
 setup(
-name         = 'journal',
-version      = journal.__version__,
-author       = journal.__author__,
-author_email = 'askedrelic@gmail.com',
+	name             = 'journal',
+	version          = journal.__version__,
+	description      = 'A CLI tool to help with keeping a work/personal journal',
+	long_description = open('README.markdown').read() + '\n\n' + open('HISTORY.markdown').read(),
 
-description      = 'A CLI tool to help with keeping a datetime organized journal',
-long_description = open('README.markdown').read(),
+	author           = journal.__author__,
+	author_email     = 'askedrelic@gmail.com',
+	url              = 'https://github.com/askedrelic/journal',
+    license          = open("LICENSE.txt").read(),
 
-url              = 'https://github.com/askedrelic/journal',
+	packages         = find_packages(),
+	entry_points     = """
+	[console_scripts]
+	journal = journal.main:main""",
+	test_suite       = 'tests',
 
-packages         = find_packages(),
-entry_points     = """
-[console_scripts]
-journal = journal.main:main""",
-test_suite       = 'tests',
+	install_requires = required,
 
-install_requires = required,
-
-classifiers      = (
-    'Intended Audience :: Developers',
-    'Natural Language :: English',
-    'Programming Language :: Python',
-    'Programming Language :: Python :: 2.5',
-    'Programming Language :: Python :: 2.6',
-    'Programming Language :: Python :: 2.7',
-    # 'Programming Language :: Python :: 3.0',
-    # 'Programming Language :: Python :: 3.1',
-),
+	classifiers      = (
+	    'Intended Audience :: Developers',
+	    'Natural Language :: English',
+	    'Programming Language :: Python',
+	    'Programming Language :: Python :: 2.5',
+	    'Programming Language :: Python :: 2.6',
+	    'Programming Language :: Python :: 2.7',
+	),
 )
 
