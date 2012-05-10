d6f07adf1cd7041749de49b97c9973b4a34e8991
Matt Behrens <askedrelic@gmail.com>
2012-05-09 23:30:05 -0700

    cleanup

--- journal/parse.py
+++ journal/parse.py
@@ -1,16 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""
-journal
-Copyright (c) 2011 Matt Behrens <askedrelic@gmail.com>
-http://asktherelic.com
-
-A CLI tool to help with keeping a datetime organized journal
-
-Licensing included in LICENSE.txt
-"""
-
 import time, datetime
 import re
 
