6c44277caad8039fec3473cfe2c43cc50d16aee6
Matt Behrens <askedrelic@gmail.com>
2012-05-09 23:37:47 -0700

    starting more work

--- journal/__init__.py
+++ journal/__init__.py
@@ -5,7 +5,7 @@
 #Copyright (c) 2012 Matt Behrens <askedrelic@gmail.com>
 #A CLI tool to help with keeping a datetime organized journal
 
-__version__ = "0.3.0"
+__version__ = "0.4.0"
 __author__  = "Matt Behrens <askedrelic@gmail.com>"
 __copyright__ = "Copyright (c) 2012 Matt Behrens <askedrelic@gmail.com>"
 
