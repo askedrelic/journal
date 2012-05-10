005293df8d9d0c94d74b31c600158aac2ae1ab56
Matt Behrens <askedrelic@gmail.com>
2012-05-09 23:20:14 -0700

    cleanup

--- journal/__init__.py
+++ journal/__init__.py
@@ -1,17 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""
-journal
-Copyright (c) 2011 Matt Behrens <askedrelic@gmail.com>
-http://asktherelic.com
+#journal
+#Copyright (c) 2012 Matt Behrens <askedrelic@gmail.com>
+#A CLI tool to help with keeping a datetime organized journal
 
-A CLI tool to help with keeping a datetime organized journal
-
-Licensing included in LICENSE.txt
-"""
-
+__version__ = "0.3.0"
 __author__  = "Matt Behrens <askedrelic@gmail.com>"
-__version__ = "0.3.0dev"
+__copyright__ = "Copyright (c) 2012 Matt Behrens <askedrelic@gmail.com>"
 
 __all__ = ["main", "parse"]
