259ad9ac970c25e6430cc3c5265ab605739e4945
Phil Snyder <subfivemile@gmail.com>
2016-06-13 19:33:39 -0700

    Python 3 compatibility added

---
+++ build/lib/journal/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#journal
+#Copyright (c) 2012 Matt Behrens <askedrelic@gmail.com>
+#A CLI tool to help with keeping a datetime organized journal
+
+__version__ = "0.5.0"
+__author__  = "Matt Behrens <askedrelic@gmail.com>"
+__copyright__ = "Copyright (c) 2012 Matt Behrens <askedrelic@gmail.com>"
+
+__all__ = ["main", "parse"]
